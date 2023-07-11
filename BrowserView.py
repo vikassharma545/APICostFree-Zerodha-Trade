7d9382366d562f892bc0d00b2809465fcdfa811d
vikas <jnv2252@gmail.com>
2023-07-11 22:12:03 +0530

    Now can can view trade on browser

---
+++ BrowserView.py
@@ -0,0 +1,30 @@
+import pyotp
+from time import sleep
+from selenium import webdriver
+from configparser import ConfigParser
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+config_object = ConfigParser()
+config_object.read("config.ini")
+userinfo = config_object["USERINFO"]
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.implicitly_wait(3)
+driver.get('https://kite.zerodha.com/')
+driver.find_element(By.XPATH, "//input[@id='userid']").send_keys(userinfo.get("userid"))
+driver.find_element(By.XPATH, "//input[@id='password']").send_keys(userinfo.get("password"))
+driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+sleep(3)
+driver.find_element(By.XPATH, "//input[@type='text']").send_keys(pyotp.TOTP(userinfo.get("totpkey")).now())
+sleep(2)
+
+enctoken = driver.get_cookie("enctoken")['value']
+
+tokeninfo = config_object["ENCTOKEN"]
+tokeninfo["enctoken"] = enctoken
+
+#Write changes back to file
+with open('config.ini', 'w') as conf:
+    config_object.write(conf)
